cdcd20567872552681c96188a0de09b24148a394
AlejCM <acm0058@alu.medac.es>
2024-01-08 12:13:04 +0100

    pruebas lavarel

--- Apuntes/Apuntes10_Laravel/series/routes/web.php
+++ Apuntes/Apuntes10_Laravel/series/routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('hola');
+});
+
+Route::get('/adios', function () {
+    return view('adios');
+});
+
+Route::get('/series', function () {
+    return view('series/index');
 });
